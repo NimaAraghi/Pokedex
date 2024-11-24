5e0b8bdd8691fc52ebfebe56efa7cab2d1e086db
Nima <nims.844@gmail.com>
2024-11-24 20:55:49 +0330

    added the Sidenav

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { first151Pokemon, getFullPokedexNumber } from '../utils/index'
 
 const SideNav = () => {
     return (
-        <div>SideNav</div>
+        <nav>
+            <div className={`header`}>
+                <h1 className='text-gradient'>Pokédex</h1>
+            </div>
+            <input type="text" />
+            {first151Pokemon.map((pokemon, pokemonIndex) => {
+                return (
+                    <button className={`nav-card`}>
+                        <p>{ getFullPokedexNumber(pokemonIndex) }</p>
+                        <p>{ pokemon }</p>
+                    </button>
+                )
+            })}
+        </nav>
     )
 }
 
